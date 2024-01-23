3d3820b1c5b5914b962db296b33b17e3782d556a
stevemesker <88290557+stevemesker@users.noreply.github.com>
2024-01-23 09:52:53 -0600

    restored dictionary search functionality
    can once again get recipe lists using the matcher. Need to have it spit back a list of the recipes I think

--- Assets/Scripts/CraftingBenches/CraftingBench_SO.cs
+++ Assets/Scripts/CraftingBenches/CraftingBench_SO.cs
@@ -21,28 +21,19 @@ public class CraftingBench_SO : SerializedScriptableObject
         Debug.Log("Building dictionary...");
         dictRecipeList = new Dictionary<Item, recipeList>(); //resets our primary dictionary list of all Recipe_SOs
 
-        //sets a temp group to hold the recipes during sorting
-        Groups = new recipeList();
-        Groups.dictRecipeList = new List<Recipes_SO>();
-
-
-        //Build List of Recipes from folders
-        //dictRecipeList.Add(recipes[0].ItemSOInput[0], Groups);
-
         while (recipes.Count > 0)
         {
+            //sets a temp group to hold the recipes during sorting
             Groups = new recipeList();
             Groups.dictRecipeList = new List<Recipes_SO>();
             Groups.dictRecipeList.Add(recipes[0]);
-            Debug.Log("Adding " + recipes[0].ItemSOInput[0] + " key to the queue. There were " + recipes.Count + " recipes..");
+
             recipes.RemoveAt(0);
-            Debug.Log("Now there are " + recipes.Count);
+
             for (int j = 0; j < recipes.Count; j++)
             {
-                //Debug.Log(Groups.dictRecipeList[0].ItemSOInput[0] + " and " + recipes[j].ItemSOInput[0]);
                 if (recipes[j].ItemSOInput[0] == Groups.dictRecipeList[0].ItemSOInput[0])
                 {
-                    Debug.LogWarning("found a match with item " + Groups.dictRecipeList[0].ItemSOInput[0] + " and " + recipes[j].ItemSOInput[0]);
                     Groups.dictRecipeList.Add(recipes[j]);
                     recipes.RemoveAt(j);
                 }
@@ -51,51 +42,6 @@ public class CraftingBench_SO : SerializedScriptableObject
             dictRecipeList.Add(Groups.dictRecipeList[0].ItemSOInput[0], Groups);
         }
 
-            /*
-            //fuction to load recipes into a dictionary since I can't see dictionaries in editor. I guaruntee there's a better way to do this but it does work
-            Debug.Log("Building dictionary...");
-            recipesCopy = new List<Recipes_SO>(recipes);
-
-            dictRecipeList = new Dictionary<Item, recipeList>();
-
-            Groups = new recipeList();
-            Groups.dictRecipeList = new List<Recipes_SO>();
-
-            Groups.dictRecipeList.Add(recipesCopy[0]);
-            recipesCopy.Remove(recipesCopy[0]);
-            //build lists
-            for (int i = 0; i < recipesCopy.Count+1; i++)
-            {
-                //Debug.Log(i+1);
-                if (i < recipesCopy.Count)
-                {
-                    if (Groups.dictRecipeList[0].ItemSOInput[0] == recipesCopy[i].ItemSOInput[0])
-                    {
-                        //Debug.Log("found a match at position " + (i+1) + " with item name " + recipesCopy[i].ItemSOInput[0].name);
-                        Groups.dictRecipeList.Add(recipesCopy[i]);
-                        recipesCopy.Remove(recipesCopy[i]);
-                        i -= 1;
-                    }
-                }
-                else
-                {
-                    //Debug.Log("adding group to dictionary");
-                    dictRecipeList.Add(Groups.dictRecipeList[0].ItemSOInput[0], Groups);
-                    if (recipesCopy.Count > 0)
-                    {
-                        //Debug.LogWarning("Inventory is still not empty");
-                        Groups = new recipeList();
-                        Groups.dictRecipeList = new List<Recipes_SO>();
-
-                        Groups.dictRecipeList.Add(recipesCopy[0]);
-                        recipesCopy.Remove(recipesCopy[0]);
-
-                        i = -1;
-                    }
-                }
-            }
-
-            Debug.Log("Dictionary Created! Now contains " + dictRecipeList.Count + " groups of recipes!"); */
     }
 
     [Button("Test for dictionary")]
@@ -115,7 +61,6 @@ public class CraftingBench_SO : SerializedScriptableObject
     [Button ("Test dictionary functionality")]
     public virtual recipeList matcher(Item indexer)
     {
-        /*
         //function that returns the group of recipes possible for an item given the item index
         
         recipeList puller = new recipeList();
@@ -127,7 +72,7 @@ public class CraftingBench_SO : SerializedScriptableObject
             return puller;
         }
         Debug.Log("Did not find recipes for " + indexer.name);
-        */
+        
         return null;
         
     }
